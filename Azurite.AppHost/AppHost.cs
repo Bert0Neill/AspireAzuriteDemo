903b7224a6da03c3af0247297e67abccb7a5b9e7
Bert O'Neill <bertoneill@yahoo.com>
2025-08-15 11:49:11 +0100

    initial prjs web & api

--- Azurite.AppHost/AppHost.cs
+++ Azurite.AppHost/AppHost.cs
@@ -1,5 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Azurite_APIs>("azurite-apis");
+builder.AddProject<Projects.Azurite_APIs>("azurite-APIs");
+builder.AddProject<Projects.Azurite_BlazorWasmApp>("azurite-BlazorWasmApp");
 
 builder.Build().Run();
