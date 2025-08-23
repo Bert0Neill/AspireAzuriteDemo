99181d3587515384ff17ed050a2962a74107902e
Bert O'Neill <bertoneill@yahoo.com>
2025-08-23 18:56:26 +0100

    ipdates

--- Azurite.AppHost/AppHost.cs
+++ Azurite.AppHost/AppHost.cs
@@ -6,7 +6,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 // Local Azure SignalR emulator
 var signalR = builder.AddConnectionString(
     "AzureSignalR",
-    "Endpoint=http://127.0.0.1:5001;AccessKey=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGH"
+    "Endpoint=http://127.0.0.1:8888;AccessKey=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGH"
 );
 
 
@@ -26,34 +26,8 @@ builder.AddProject<Projects.Azurite_APIs>("webapi")
 // Reference your Blazor WASM project
 builder.AddProject<Projects.Azurite_BlazorWasmApp>("azurite-BlazorWasmApp");
 
-
-//// Add Azure Service Bus and configure to run as emulator
-//var serviceBus = builder.AddAzureServiceBus("myservicebus")
-//                        .RunAsEmulator(emulator =>
-//                        {
-//                            emulator.WithConfigurationFile(@"config\servicebus-config.json");
-//                        });
-
-//// Add Service Bus queue or topic
-//serviceBus.AddServiceBusQueue("myqueue");
-
-
-
-//// Service Bus Emulator container
-//var sbEmu = builder.AddContainer("servicebus-emulator", "mcr.microsoft.com/azure-messaging/service-bus-emulator:latest")
-//    .WithEnvironment("ACCEPT_EULA", "Y")
-//    .WithEnvironment("MSSQL_SA_PASSWORD", "YourStrong!Pass123")
-//    .WithEndpoint(name: "sb", targetPort: 5672, isProxied: false) // AMQP
-//    .WithBindMount("./config.json", "/ServiceBus_Emulator/config.json"); // your queues/topics config
-
-// Minimal API service
-// add references to projects that you wish to view in Aspire orchestrator
-//builder.AddProject<Projects.Azurite_APIs>("azurite-APIs")
-//    .WithEnvironment("ServiceBus__ConnectionString",
-//        "Endpoint=sb://servicebus-emulator/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=DummyKey;UseDevelopmentEmulator=true;");
-
-
-//var serviceBus = builder.AddAzureServiceBus("DemoAzuriteServiceBus").RunAsEmulator().AddServiceBusQueue("DemoPoliciesQueue");
-//builder.AddProject<Projects.ConsoleAppServiceBusClient>("azurite-ConsoleAppServiceBusClient").WithReference(serviceBus).WaitFor(serviceBus);
+// Reference your Console App Service Bus Client project
+builder.AddProject<Projects.ConsoleAppServiceBusClient>("azurite-ConsoleAppServiceBusClient")
+       .WithReference(serviceBus);
 
 builder.Build().Run();
